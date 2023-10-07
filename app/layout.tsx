554b56450a37ac91fe54ef2470b75372165fe0d2
Nishika Pabba <nishika.pabba@gmail.com>
2023-10-07 15:24:44 -0400

    connected webapp to database

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { SessionProvider } from "next-auth/react"
+import { NextAuthProvider } from './NextAuthProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,8 +17,13 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
+    
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <NextAuthProvider>
+      {children}
+        </NextAuthProvider>
+      </body>
     </html>
   )
 }
