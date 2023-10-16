0f252510fea1b33e3efc33917392751a672882f2
Naomi Gillis <naomigillis@Naomis-MacBook-Pro.local>
2023-10-16 19:02:38 -0400

    Created Button

--- app/page.tsx
+++ app/page.tsx
@@ -1,2 +1,2 @@
-
-export default function Home(){return <div>hi !</div>}
+import Button from '@mui/material/Button';
+export default function Home(){return <div> <Button variant="outlined" color="secondary" size="large">Login</Button></div>}
