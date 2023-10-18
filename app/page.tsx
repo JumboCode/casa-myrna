701fc55333cdc1c19e9798cfef64c0f1276f68f9
Maddie Rogers <maddierogers@Maddies-MacBook-Pro-2.local>
2023-10-18 17:45:03 -0400

    button

--- app/page.tsx
+++ app/page.tsx
@@ -1,2 +1,14 @@
 
-export default function Home(){return <div>hi !</div>}
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+
+const buttonStyle = {
+    fontFamily: "Arial"
+};
+export default function ButtonUsage() {
+  return <Box textAlign='center'>
+    <Button variant="contained" color="secondary" style={buttonStyle}> LOGIN </Button>
+        </Box>;
+ 
+}
