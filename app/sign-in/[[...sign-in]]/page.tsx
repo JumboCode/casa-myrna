598e1a1a80aae51e9f6d2e0193a79890559be150
Carly Seigel <Carly@MacBook-Pro-37.local>
2023-10-28 16:32:19 -0400

    centered sign in button and added logo. still having trouble modifying the text in the clerkprovided sign in box.

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,55 +1,57 @@
-import { ClerkProvider, SignIn } from "@clerk/nextjs";
+import { SignIn } from "@clerk/nextjs";
 import { AppProps } from "next/app";
 import React from "react";
+import Image from "next/image";
+import logo from "app/images/logo.svg";
 
 function MyApp({ pageProps }: AppProps) {
   return (
-    <div style={{background: 'linear-gradient(180deg, #C3DC98 42.71%, rgba(108, 187, 227, 0.99) 96.72%, rgba(108, 187, 227, 0) 99.99%), linear-gradient(180deg, #C3DC98 42.71%, rgba(108, 187, 227, 0.99) 96.72%, rgba(108, 187, 227, 0) 99.99%)', 
-          height: '750px', 
-          border:'0'} }>
-            <>
-            <SignIn
-              appearance={{
-                elements: {
-                  justifyContent: "center",
-                  formButtonPrimary: {
-                    fontSize: 30,
-                    textTransform: "none",
-                    backgroundColor: "#611BBD",
-                    "&:hover, &:focus, &:active": {
-                      backgroundColor: "#49247A",
-                    },
-                  },
-                },
-              }}
-            />
-            </>
+    <div
+      style={{
+        background:
+          "linear-gradient(180deg, #C3DC98 42.71%, rgba(108, 187, 227, 0.99) 96.72%, rgba(108, 187, 227, 0) 99.99%), linear-gradient(180deg, #C3DC98 42.71%, rgba(108, 187, 227, 0.99) 96.72%, rgba(108, 187, 227, 0) 99.99%)",
+        height: "750px",
+        justifyContent: "center",
+        alignItems: "center",
+        className: "background",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          paddingTop: "100px",
+        }}
+      >
+        <Image src={logo} alt="Error" />
+      </div>
 
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <SignIn
+          appearance={{
+            elements: {
+              justifyContent: "center",
+              formButtonPrimary: {
+                fontSize: 80,
+                textTransform: "none",
+                backgroundColor: "#611BBD",
+                "&:hover, &:focus, &:active": {
+                  backgroundColor: "#49247A",
+                },
+              },
+            },
+          }}
+        />
+      </div>
     </div>
   );
 }
- 
+
 export default MyApp;
-
-
-/* Rectangle 34 */
-
-// position: absolute;
-// width: 1280px;
-// height: 749px;
-// left: 0px;
-// top: 105px;
-
-/* Rectangle 32 */
-
-// position: absolute;
-// width: 636px;
-// height: 442px;
-// left: 322px;
-// top: 195px;
-
-// background: #FFFFFF;
-// border-radius: 61px;
-
-
-// background: linear-gradient(180deg, #C3DC98 42.71%, rgba(108, 187, 227, 0.99) 96.72%, rgba(108, 187, 227, 0) 99.99%), linear-gradient(180deg, #C3DC98 42.71%, rgba(108, 187, 227, 0.99) 96.72%, rgba(108, 187, 227, 0) 99.99%);
