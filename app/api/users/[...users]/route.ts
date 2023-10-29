cba8fb46c67eaaa8a49a21472076ad228ea60a1d
Jstark67 <109922840+Jstark67@users.noreply.github.com>
2023-10-28 22:00:49 -0400

    skeleton for user endpoint

---
+++ app/api/users/[...users]/route.ts
@@ -0,0 +1,10 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+        const requestMethod = req.method;
+        const body = JSON.parse(req.body);
+      switch (requestMethod){
+        case
+      } 
+        
+}
