25b82795d9f0292d40b2977eced8ce25e9e2151b
Eliana Longoria-Valenzuela <elongoria-valenzuela23@students.polytechnic.org>
2023-11-05 09:58:47 -0500

    corrected formmating of sidebar + hamberger icon

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -7,8 +7,10 @@ import Announcements from "app/images/2.png";
 import Calendar from "app/images/3.png";
 import Groupchat from "app/images/4.png";
 import Profile from "app/images/7.png";
+import Logout from "app/images/5.png";
 import Image from "next/image";
 import Logo from "app/images/1.png";
+import Hamburger from "app/images/hamburger.png";
 
 
 const SideNav = () => {
@@ -20,24 +22,45 @@ const SideNav = () => {
         height: "750px",
         alignItems: "center",
         justifyContent: "center"
-      }}>  
-      <div style = {{display: "flex",alignItems: "center",
-        justifyContent: "center"}}><Image src = {Logo} alt = "Error" width = {120} height= {120} /> </div>
+      }}>
+        <MenuList>
+          <MenuItem component = 'a' href = '' >  
+          <Image src = {Hamburger} alt = "Error" width = {20} height= {20}/>
+          </MenuItem>
+        </MenuList>
+      
 
-         <MenuList>
+      <div style = {{paddingTop: 55, display: "flex",alignItems: "center",
+        justifyContent: "center"}}><Image src = {Logo} alt = "Error" width = {160} height= {160} /> </div>
 
-            <MenuItem component = 'a' href = 'calendar' >  
+         <MenuList style= {{paddingTop: 100}}>
+            <MenuItem style = {{background: "#FFFFFF", borderRadius: 20, color:"#2E0057"}} component = 'a' href = 'calendar' >  
             <Image src = {Calendar} alt = "Error" width = {30} height= {30}/>
             Calendar </MenuItem>
-            <MenuItem component = 'a' href = 'announcements'>
+
+            <div style = {{paddingBottom: 15, background: "#F6F6F6"}}></div>
+
+            <MenuItem style = {{background: "#FFFFFF", borderRadius: 20, color:"#2E0057"}} component = 'a' href = 'announcements'>
             <Image src = {Announcements} alt = "Error" width = {30} height= {30}/>
                Announcements </MenuItem>
-            <MenuItem component = 'a' href = 'group-chat'> 
+            <div style = {{paddingBottom: 15, background: "#F6F6F6"}}></div>
+
+            <MenuItem style = {{background: "#FFFFFF", borderRadius: 20, color:"#2E0057"}} component = 'a' href = 'group-chat'> 
             <Image src = {Groupchat} alt = "Error" width = {30} height= {30}/>
             Groupchat </MenuItem>
-            <MenuItem component = 'a' href = 'my-profile'>  
+            <div style = {{paddingBottom: 15, background: "#F6F6F6"}}></div>
+
+            <MenuItem style = {{background: "#FFFFFF", borderRadius: 20, color:"#2E0057"}} component = 'a' href = 'my-profile'>  
             <Image src = {Profile} alt = "Error" width = {30} height= {30}/>
             Profile</MenuItem>
+        </MenuList>
+
+         <MenuList style= {{paddingTop: 75, alignItems: "center",
+        justifyContent: "center",display: "flex",color:"#2E0057"}}>  
+          <MenuItem component = 'a' href = '' >  
+              <Image src = {Logout} alt = "Error" width = {30} height= {30}/>
+              Logout 
+            </MenuItem>
         </MenuList>
       </div>
       <div style = {{background: "linear-gradient(180deg, #C3DC98 42.71%, rgba(108, 187, 227, 0.99) 96.72%, rgba(108, 187, 227, 0) 99.99%), linear-gradient(180deg, #C3DC98 42.71%, rgba(108, 187, 227, 0.99) 96.72%, rgba(108, 187, 227, 0) 99.99%)",
@@ -54,52 +77,3 @@ const SideNav = () => {
 
 export default SideNav;
 
-
-// export default function sidebar() {
-//   const [open, setOpen] = React.useState(false);
-//   const anchorRef = React.useRef(null);
-
-//   // const handleToggle = () => {
-//   //   setOpen((prevOpen) => !prevOpen);
-//   // };
-
-//   const handleClose = (event) => {
-//     if (anchorRef.current && anchorRef.current.contains(event.target)) {
-//       return;
-//     }
-
-//     setOpen(false);
-//   };
-
-//   // function handleListKeyDown(event) {
-//   //   if (event.key === 'Tab') {
-//   //     event.preventDefault();
-//   //     setOpen(false);
-//   //   } else if (event.key === 'Escape') {
-//   //     setOpen(false);
-//   //   }
-//   // }
-
-//   // return focus to the button when we transitioned from !open -> open
-//   const prevOpen = React.useRef(open);
-//   React.useEffect(() => {
-//     if (prevOpen.current === true && open === false) {
-//       anchorRef.current.focus();
-//     }
-
-//     prevOpen.current = open;
-//   }, [open]);
-
-//   return (
-//     <Stack direction="row" spacing={2}>
-//       <Paper>
-//       <MenuList>
-//           <MenuItem>Calendar</MenuItem>
-//           <MenuItem>Announcements</MenuItem>
-//           <MenuItem>Groupchat</MenuItem>
-//           <MenuItem>Profile</MenuItem>
-//         </MenuList>
-//       </Paper>
-//     </Stack>
-//   );
-// }
