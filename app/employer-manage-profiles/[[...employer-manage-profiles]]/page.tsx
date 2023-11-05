9d7555cf7424511c416fc356de7ac9abe4a427ba
Naomi Gillis <naomigillis@Naomis-MacBook-Pro.local>
2023-11-05 17:58:58 -0500

    pr for managing the profile

---
+++ app/employer-manage-profiles/[[...employer-manage-profiles]]/page.tsx
@@ -0,0 +1,75 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Stack from '@mui/system/Stack';
+import Typography from '@mui/material/Typography';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import { Transform } from '@mui/icons-material';
+import CreateIcon from '@mui/icons-material/Create';
+import IconButton from '@mui/material/IconButton';
+import sidebar from "../../components/sidebar"; 
+
+
+
+
+export default function Employer({firstName, lastName, role, imageUrl}){
+  return (
+   
+    <Box
+    sx={{
+      display: 'flex',
+      bgcolor: 'silver',
+      boxShadow: 1,
+      borderRadius: 4,
+      height: '650px',
+      minWidth: 250,
+     
+      p:5,
+      justifyContent: 'flex-end',
+    }}>
+    
+    <Box
+     
+    sx={{
+      bgcolor: 'background.paper',
+      boxShadow: 1,
+      borderRadius: 2,
+      width: '1000px',
+      height: '40px',
+      justifyContent: 'center',
+    }}> 
+        <Stack direction="row" spacing={20} justifyContent={'space-around'}>
+        
+            <Stack direction="row" spacing={3} justifyContent={'space-around'} alignItems={'center'}>
+
+            <Avatar  alt="Remy Sharp" src={imageUrl} sx={{ alignItems:'center', width: 25, height: 25, }}/>
+
+            <Typography textAlign='center' sx={{typography: 'body1', 
+                                                             fontSize: 24,}}>
+                              {firstName}
+            </Typography>
+            </Stack>
+
+            <Typography variant="h4" textAlign='center' sx={{typography: 'body1', 
+                                                             fontSize: 24,}} >
+                                {lastName}
+            </Typography>
+
+            <Typography variant="h4" textAlign='center' sx={{typography: 'body1', 
+                                                             fontSize: 24,}} >
+                                {role}
+            </Typography>
+            <Stack direction="row" spacing={0} justifyContent={'space-around'} alignItems={'center'}>
+            <Button color="secondary"> edit </Button>
+            <IconButton color="secondary" aria-label="add an create">
+            <CreateIcon />
+          </IconButton>
+
+            </Stack>
+
+            
+        </Stack> </Box>
+  </Box>
+  )
+}
+
