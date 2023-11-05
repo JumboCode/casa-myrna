11740432578b69445e5b391529eaf3ec7c2986f0
Naomi Gillis <naomigillis@Naomis-MacBook-Pro.local>
2023-11-05 17:58:58 -0500

    pr for managing the profile

---
+++ app/components/profileList.tsx
@@ -0,0 +1,14 @@
+
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+
+
+const ProfileList = () => {
+  return(
+    <div className = "profilelist">hi 
+    </div>iv>
+  ); 
+};
+
+
+export default ProfileList;
