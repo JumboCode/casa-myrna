cf9c9a1c47c37287ccdce3787311e85d4b018d15
Jiyoon Choi <jiyoonchoi15@gmail.com>
2023-11-08 16:07:36 -0500

    added the applied filters object

--- app/components/manage-profiles.tsx
+++ app/components/manage-profiles.tsx
@@ -3,7 +3,9 @@ import Box from '@mui/material/Box';
 import Grid from '@mui/material/Unstable_Grid2' ;
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
-import Autocomplete from '@mui/material/Autocomplete';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
+import Autocomplete, { createFilterOptions } from '@mui/material/Autocomplete';
 import Button from '@mui/material/Button';
 import profileList from "./profileList"
 
@@ -18,32 +20,48 @@ const BoxSx = () => {
         borderRadius: '5%',
         backgroundColor: '#f6f6f6',
         fontFamily: 'default',
-        margin: '2%'
+        margin: '2%',
+        marginLeft: '5%',
+        marginRight: '5%'
       }}
     >
       {/* everything above the profile list */}
-      <Grid container spacing={5} columnSpacing={{xs: 20, sm:80, md:5, lg:5}} columns={12} margin={8}>
-        <div class='headerContent'>
-          <Grid container spacing={6} columns={20} paddingBottom='10%'>
+      <Grid container spacing={3} columnSpacing={{xs: 20, sm:80, md:5, lg:5}} margin={10}>
+        <div className='headerContent'>
+          <Grid container spacing={3} columns={20} paddingBottom='10%'>
             {/* Left side of header */}
-            <Grid item xs={14} paddingRight='15%'>
+            <Grid item xs={12}>
               {/* Page Title */}
               <Typography variant="h4" justifyContent='center' sx={{fontWeight: 'bold', paddingBottom:'10%'}}>
                   Manage Profiles
               </Typography>
 
               {/* Search Bar */}
-              <Grid container spacing={2} columns={4}>
+              <Grid container spacing={3} columns={4}>
                 {/* Actual Search Bar */}
                 <Grid item xs={3}>
-                  <TextField fullWidth defaultValue="Search" InputProps={{readOnly: true }} sx={{backgroundColor: '#FFFFFF', borderRadius:'10px'}} id="filled-basic" label="" variant="standard"/>
+                  <TextField fullWidth InputProps={{readOnly: true }} sx={{backgroundColor: '#FFFFFF', borderRadius:'10px', padding:'4px'}} id="filled-basic" label="" variant="standard"/>
                 </Grid>
                 {/* Search Button */}
                 <Grid item xs={1}>
                   <Button fullWidth sx={{borderRadius:'15px', backgroundColor:"#89B839", '&:hover': {backgroundColor:"#89B839"}, textTransform: 'none'}}variant="contained">search</Button>
                 </Grid>
               </Grid>
+
+              {/* Applied filters */}
+              <Grid container spacing={2} item xs={12} paddingTop='2%' alignItems="center">
+                <Grid item xs={2}>
+                  applied filters:
+                </Grid>
+                <Grid item xs={10}>
+                  <TextField defaultValue="All" InputProps={{ readOnly: true }} sx={{ backgroundColor: '#FFFFFF', borderRadius: '10px' }} id="filled-basic" label="" variant="standard" />
+                </Grid>
+              </Grid>
+
+
             </Grid>
+
+            <Grid item xs={1} />
             
             {/* Right side of header */}
             <Grid item xs={6}>
@@ -55,10 +73,22 @@ const BoxSx = () => {
               </Grid>
 
               {/* Select filters */}
+              {/* MUI Multiple values: https://mui.com/material-ui/react-autocomplete/#multiple-values */}
               <Grid container spacing={2} columns={3}>
                 <Grid item xs={2}>
-                  <TextField fullWidth defaultValue="Select filters" InputProps={{readOnly: true }} sx={{backgroundColor: '#FFFFFF', borderRadius:'10px'}} id="filled-basic" label="" variant="standard"/>
+                  <Select
+                    fullWidth
+                    value="" // Set the initial value to an empty string
+                    displayEmpty // Display the selected value even when it's empty
+                    sx={{ backgroundColor: '#FFFFFF', borderRadius: '10px', height: '38px'}}
+                  >
+                    <MenuItem value="" disabled>
+                      Select filters
+                    </MenuItem>
+                    {/* Add more MenuItem components with filter options here */}
+                  </Select>
                 </Grid>
+                {/* Filter button */}
                 <Grid item xs={1}>
                   <Button fullWidth sx={{borderRadius:'15px', backgroundColor:"#89B839", '&:hover': {backgroundColor:"#89B839"}, textTransform: 'none'}}variant="contained">filter</Button>
                 </Grid>
@@ -72,10 +102,8 @@ const BoxSx = () => {
               {profileList({firstName: "N", lastName: "P", role: "volunteer", imageUrl: "image"})}
             </Grid>
           </div>
-
+          
         </div>
-        
-        
       </Grid>
   </Box>);
 }
