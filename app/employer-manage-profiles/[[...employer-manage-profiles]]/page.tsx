1dd70577b285cfd1dc84008a392ee78bb18c006d
Naomi Gillis <naomigillis@Naomis-MacBook-Pro.local>
2023-11-09 17:50:58 -0500

    we tried

--- app/employer-manage-profiles/[[...employer-manage-profiles]]/page.tsx
+++ app/employer-manage-profiles/[[...employer-manage-profiles]]/page.tsx
@@ -1,5 +1,37 @@
 import profileList from "../../components/profileList"
+import React from "react"
+const names = [ 
+  {
+    name: "Maddy",
+    lastName: "Rogers",
+    role: "volunteer",
+    image: "image",
 
-export default function Page() {
-  return profileList({firstName: "N", lastName: "P", role: "volunteer", imageUrl: "image"});
-}
+  },
+  {
+    name: "Naomi",
+    lastName: "Gillis",
+    role: "volunteer",
+    image: "image",
+
+  },
+  {
+    name: "Elliana",
+    lastName: "Longoria-Valenzuela",
+    role: "volunteer",
+    image: "image",
+
+  }
+]
+// export default function Page(any: hello) {
+//   return hello 
+// }
+console.log(
+<div>
+    for(var i = 0; i < names.length; i++)
+      {
+        var x = profileList({firstName: names[i].name, lastName: names[i].lastName, role: names[i].role, imageUrl: names[i].image});
+      }
+  </div>
+)
+return ()
