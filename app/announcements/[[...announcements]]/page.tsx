7838a9a9182353f2b78ec31e00f05da538c872e6
Naomi Gillis <naomigillis@Naomis-MacBook-Pro.local>
2023-11-15 20:45:09 -0500

    worked on mobile a little

--- app/announcements/[[...announcements]]/page.tsx
+++ app/announcements/[[...announcements]]/page.tsx
@@ -1,3 +1,7 @@
 import SideNav from "@/app/components/sidebar"
+import announcements from "../../components/announcements"
 
-export default function Home(){return <SideNav></SideNav>}
+export default function Home()
+{
+    return announcements({senderName:"Carly", senderEmail:"carly@gmail.com", messageTitle:"hi", imageUrl:"image", date:"11/15"})
+}
