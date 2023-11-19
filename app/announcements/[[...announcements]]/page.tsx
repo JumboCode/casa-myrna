7ec493a85051332fd4a0171ab4c20449176005d8
pmelga01 <pamela.melgar@tufts.edu>
2023-11-18 20:21:36 -0500

    work in progress with Maddi

--- app/announcements/[[...announcements]]/page.tsx
+++ app/announcements/[[...announcements]]/page.tsx
@@ -1,6 +1,7 @@
 
 "use client"
 import { FC, useState } from 'react'
+import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
 
 import SideNav from "@/app/components/sidebar"
 
@@ -9,20 +10,12 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
+import Avatar from '@mui/material/Avatar';
+import Stack from '@mui/material/Stack';
 
-const style = {
-  position: 'absolute' as 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 800,
-  height: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
- 
-};
+
+const color = "#f6f6f6"; 
+
 
 
 // import * as React from 'react';
@@ -48,13 +41,28 @@ const style = {
 //   );
 // }
 
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 800,
+  height: 400,
+  bgcolor: "#f6f6f6",
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+  borderRadius: '50px',
+ 
+};
+
 export default function Home() {
     const [open, setOpen] = React.useState(false);
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
     return (
         <div>
-        <Button onClick={handleOpen}>Open modal</Button>
+        <Button onClick={handleOpen}>View Announcement</Button>
         <Modal
         open={open}
         onClose={handleClose}
@@ -62,14 +70,38 @@ export default function Home() {
         aria-describedby="modal-modal-description">
         <Box sx={style}>
             <Box sx={{width: '50px', height: '50px', position: 'absolute', right: '5%'}}>
-            help
+            <button
+              onClick={() => {
+              handleClose();
+               }}
+           ><CloseOutlinedIcon />
+           </button>
             </Box>
           <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
+            This is the Announcements Title
           </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
+          <Box
+            sx={{
+                display: 'flex',
+                minHeight: "350px",
+                borderRadius: '20px', //makes rounded corners
+                backgroundColor: '#ffffff', //color is variable established above! (grey!)
+                fontFamily: 'default',
+                margin: '2%',
+                
+            }}>
+                <Stack direction="row" spacing={5}>
+                    <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" sx={{ width: 50, height: 50, ml: 5, mt: 3}} />
+
+                    <Typography id="modal-modal-description" sx={{ mt: 5 }}>
+                    Sender Name
+                    </Typography>   
+                <Grid justifyContent= 'flex-end'></Grid>
+                    <Typography id="modal-modal-description" sx={{ mt: 2}} >
+                        12 November 2023
+                    </Typography>
+                </Stack>                
+            </Box>
         </Box>
       </Modal>
         </div>
