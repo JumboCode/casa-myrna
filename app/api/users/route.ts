9e58b870c1399b31b20852aaa4d191c9287a5feb
Jstark67 <109922840+Jstark67@users.noreply.github.com>
2023-11-19 15:28:33 -0500

    first try

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,4 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { clerkClient } from '@clerk/nextjs';
 import { type NextRequest } from 'next/server'
 import { PrismaClient, Prisma } from '@prisma/client'
 const prisma = new PrismaClient()
@@ -8,34 +9,51 @@ const prisma = new PrismaClient()
  * Expects an integer id to be provided as a query parameter 
  */
 export async function GET(req: NextRequest) {
-  try {
-    const searchParams = req.nextUrl.searchParams
-    let idString = searchParams.get('id')
-    let idNum = parseInt(idString as string, 10) // 10 = base 10 
 
-    if ((isNaN(idNum))) {
-      return new Response('Error: Please specify an integer user id', {
+    const searchParams = req.nextUrl.searchParams
+    let idString = searchParams.get('username')
+    // let idNum = parseInt(idString as string, 10) // 10 = base 10 
+    if (!idString) {
+        return new Response('Error: Please specify a username', {
         status: 400,
       })
     }
 
-    const user = await prisma.user.findUnique({
-      where: {
-        id: idNum,
-      },
-    })
-
-    if (!user) {
-      return new Response('Error: User not found', {
+    try {
+      const user = await clerkClient.users.getUserList({
+        username: [idString],
+      });
+      return Response.json(user[0])
+    } catch {
+      return new Response('Error: user not found', {
         status: 404,
       })
     }
-    return Response.json(user)
- } catch {
-     return new Response('Error: An unexpected error occured', {
-       status: 500,
-     })
- }
+  
+
+//     if ((isNaN(idNum))) {
+//       return new Response('Error: Please specify an integer user id', {
+//         status: 400,
+//       })
+//     }
+
+//     const user = await prisma.user.findUnique({
+//       where: {
+//         id: idNum,
+//       },
+//     })
+
+//     if (!user) {
+//       return new Response('Error: User not found', {
+//         status: 404,
+//       })
+//     }
+//     return Response.json(user)
+//  } catch {
+//      return new Response('Error: An unexpected error occured', {
+//        status: 500,
+//      })
+//  }
 }
 
 /* 
@@ -45,97 +63,109 @@ export async function GET(req: NextRequest) {
  * to the fields in the user model in schema.prisma
  */
 export async function POST(req: Request) {
-  try {
-    let data = await req.json();
-    data = {
-      ...data,
-      "created_at": new Date(data.created_at)
-    };
-    const user = await prisma.user.create({data});
+  // try {
+    // let data = await req.json();
+    // data = {
+    //   ...data,
+    //   "created_at": new Date(data.created_at)
+    // };
+    // let data = {
+    //   "externalId?": "a"
+    // }
+    const user = await clerkClient.users.createUser({
+      emailAddress: ['user@example.com'],
+      
+      // externalId: '123456',
+      username: 'newuser1235',
+      password: 'aStrongPassword',
+      firstName: 'John',
+      lastName: 'Doe',
+      // phoneNumber: ['+12025550108'],
+    });
     return new Response(JSON.stringify(user))
-  } catch (error){
-    if (error instanceof Prisma.PrismaClientValidationError) {
-      return new Response('Error: ' + error.message, {status: 400,})
-    } else {
-      return new Response('Error: An unexpected error occured', {status: 500,})
-    }
-  }
+  // } catch (error){
+  //   if (error instanceof Prisma.PrismaClientValidationError) {
+  //     return new Response('Error: ' + error.message, {status: 400,})
+  //   } else {
+  //     return new Response('Error: An unexpected error occured', {status: 500,})
+  //   }
+  // }
 }
 
-/* 
- * Upserts a user into the database
- * Expects an integer id to be provided as a query parameter 
- * Expects the request body to be json with the fields username, firstName, 
- * lastName, pronouns, role, and created_at, with these fields corresponding 
- * to the fields in the user model in schema.prisma
- */
-export async function PUT(req: NextRequest) {
-  try {
-    const searchParams = req.nextUrl.searchParams
-    let idString = searchParams.get('id')
-    let idNum = parseInt(idString as string, 10) // 10 = base 10 
-    if ((isNaN(idNum))) {
-      return new Response('Error: Please specify an integer user id', {
-        status: 400,
-      })
-    }
+// /* 
+//  * Upserts a user into the database
+//  * Expects an integer id to be provided as a query parameter 
+//  * Expects the request body to be json with the fields username, firstName, 
+//  * lastName, pronouns, role, and created_at, with these fields corresponding 
+//  * to the fields in the user model in schema.prisma
+//  */
+// export async function PUT(req: NextRequest) {
+//   try {
+//     const searchParams = req.nextUrl.searchParams
+//     let idString = searchParams.get('id')
+//     let idNum = parseInt(idString as string, 10) // 10 = base 10 
+//     if ((isNaN(idNum))) {
+//       return new Response('Error: Please specify an integer user id', {
+//         status: 400,
+//       })
+//     }
 
-    let userData = await req.json();
-    userData = {
-      ...userData,
-      "id": idNum,
-      "created_at": new Date(userData.created_at)
-    };
+//     let userData = await req.json();
+//     userData = {
+//       ...userData,
+//       "id": idNum,
+//       "created_at": new Date(userData.created_at)
+//     };
 
-    const user = await prisma.user.upsert({
-      where: {
-        id: userData.id,
-      },
-      update: userData,
-      create: userData
-    })
-    return new Response(JSON.stringify(user))
- } catch (error){
-     if (error instanceof Prisma.PrismaClientValidationError) {
-       return new Response('Error: ' + error.message, {status: 400,})
-     } else {
-       return new Response('Error: An unexpected error occured', {status: 500,})
-     }
-  }
-}
+//     const user = await prisma.user.upsert({
+//       where: {
+//         id: userData.id,
+//       },
+//       update: userData,
+//       create: userData
+//     })
+//     return new Response(JSON.stringify(user))
+//  } catch (error){
+//      if (error instanceof Prisma.PrismaClientValidationError) {
+//        return new Response('Error: ' + error.message, {status: 400,})
+//      } else {
+//        return new Response('Error: An unexpected error occured', {status: 500,})
+//      }
+//   }
+// }
 
 
-/* 
- * DELETEs a user from the database
- * Expects an integer id to be provided as a query parameter 
- */
-export async function DELETE(req: NextRequest) {
-  try {
-    const searchParams = req.nextUrl.searchParams
-    let idString = searchParams.get('id')
-    let idNum = parseInt(idString as string, 10) // 10 = base 10 
+// /* 
+//  * DELETEs a user from the database
+//  * Expects an integer id to be provided as a query parameter 
+//  */
+// export async function DELETE(req: NextRequest) {
+//   try {
+//     const searchParams = req.nextUrl.searchParams
+//     let idString = searchParams.get('id')
+//     let idNum = parseInt(idString as string, 10) // 10 = base 10 
 
-    if ((isNaN(idNum))) {
-      return new Response('Error: Please specify an integer user id', {
-        status: 400,
-      })
-    }
+//     if ((isNaN(idNum))) {
+//       return new Response('Error: Please specify an integer user id', {
+//         status: 400,
+//       })
+//     }
 
-    const user = await prisma.user.delete({
-      where: {
-        id: idNum,
-      },
-    })
+//     const user = await prisma.user.delete({
+//       where: {
+//         id: idNum,
+//       },
+//     })
 
-    return Response.json(user)
- } catch (error) {
-     if (error instanceof Prisma.PrismaClientKnownRequestError && 
-         error.code === 'P2025') {
-      return new Response('Error: Error: User not found', {
-        status: 400,
-      })
-    } else {
-        return new Response('Error: An unexpected error occured', {status: 500,})
-    }
-    }   
- }
+//     return Response.json(user)
+//  } catch (error) {
+//      if (error instanceof Prisma.PrismaClientKnownRequestError && 
+//          error.code === 'P2025') {
+//       return new Response('Error: Error: User not found', {
+//         status: 400,
+//       })
+//     } else {
+//         return new Response('Error: An unexpected error occured', {status: 500,})
+//     }
+//     }   
+//  }
