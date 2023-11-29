090d72e9873a20f3af9f664cde2aafc8af5523d9
Jiyoon Choi <jiyoonchoi15@gmail.com>
2023-11-29 16:11:03 -0500

    built up announcements page

--- app/announcements/[[...announcements]]/page.tsx
+++ app/announcements/[[...announcements]]/page.tsx
@@ -2,13 +2,14 @@
 
 import Sidebar from "../../components/Sidebar"; 
 import React, { useState, useEffect, ReactElement } from "react";
+import AnnouncementList from "../../components/AnnouncementList";
 import AnnouncementsModalButton from "../../components/AnnouncementsModalButton";
 import "../../globals.css";
 import { ThemeProvider } from '@mui/material/styles';
 import theme from '../../theme';
 
 const Announcements: React.FC = ()  => {
-  return (<ThemeProvider theme={theme}><Sidebar currentPageComponent={AnnouncementsModalButton}/></ThemeProvider>)
+  return (<ThemeProvider theme={theme}><Sidebar currentPageComponent={AnnouncementList}/></ThemeProvider>)
 };
 
 export default Announcements;
