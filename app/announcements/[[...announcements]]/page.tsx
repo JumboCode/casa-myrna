87e098ba135e26327ec83b1444ed99ce1e668ce6
Carly Seigel <Carly@MacBook-Pro-37.local>
2023-12-10 14:10:41 -0500

    reconfigured announcement component and announcementList component. Fixed scaling issue.

--- app/announcements/[[...announcements]]/page.tsx
+++ app/announcements/[[...announcements]]/page.tsx
@@ -4,6 +4,7 @@ import Sidebar from "../../components/Sidebar";
 import React, { useState, useEffect, ReactElement } from "react";
 import AnnouncementsModalButton from "../../components/AnnouncementsModalButton";
 import AnnouncementList from "../../components/AnnouncementList";
+import Announcement from "../../components/announcements";
 import "../../globals.css";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "../../theme";
@@ -13,6 +14,7 @@ const Announcements: React.FC = () => {
     <ThemeProvider theme={theme}>
       {/* <Sidebar currentPageComponent={AnnouncementsModalButton} /> */}
       <Sidebar currentPageComponent={AnnouncementList} />
+      {/* <Sidebar currentPageComponent={Announcement} /> */}
     </ThemeProvider>
   );
 };
