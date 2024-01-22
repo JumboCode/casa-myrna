f5bdb6fc00277b7cdcf1e00b3ce7ff71011ad2d8
Bill Soronzonbold <Soronzonboldbill@gmail.com>
2024-01-21 20:08:12 -0500

    frontend for shifts done

--- app/calendar/[[...calendar]]/page.tsx
+++ app/calendar/[[...calendar]]/page.tsx
@@ -1,15 +1,43 @@
 'use client'
 
 import { ThemeProvider } from '@mui/material/styles';
-import React from "react";
+import React, { useState, useEffect } from "react";
 import CalendarModalButton from "../../components/CalendarModalButton";
 import Sidebar from "../../components/Sidebar";
 import "../../globals.css";
 import theme from '../../theme';
 
 const Calendar: React.FC = () => {
+  const [shiftArray, setShiftArray] = useState<any>(null);
+  useEffect(() => { setShiftArray(null); }, [])
 
-  return (<ThemeProvider theme={theme}><Sidebar currentPageComponent={CalendarModalButton} /></ThemeProvider>)
+  const fetchShiftData = async (startTime: Date, endTime: Date) => {
+    try {
+      const response = await fetch('api/shifts');
+      if (!response.ok) {
+        throw new Error('Failed to fetch shift data');
+      }
+      const data = await response.json();
+      setShiftArray(data);
+
+    } catch (error) {
+      console.error('Error fetching shift data:', error);
+    }
+  };
+
+  return (<ThemeProvider theme={theme}><Sidebar currentPageComponent={CalendarModalButton} />
+    <div className="post-btn">
+      <button className="post-btn-retrieve" onClick={() => fetchShiftData(new Date("2019-01-01"), new Date("2024-01-01"))}>retrieve info</button>
+
+      {(!shiftArray) ? (
+        <></>
+      ) : (
+        <div className="">
+          results here
+        </div>
+      )}
+    </div>
+  </ThemeProvider>)
 };
 
 export default Calendar;
--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,10 @@ import { NextResponse } from "next/server";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
 export default authMiddleware({
-      publicRoutes: ["/login", "/sign-up"],
+      /******************************************
+       * todo: remove the api route from public * 
+       ******************************************/ 
+      publicRoutes: ["/login", "/sign-up", "api/shifts"],
 
       afterAuth(auth, req, evt) {
 
