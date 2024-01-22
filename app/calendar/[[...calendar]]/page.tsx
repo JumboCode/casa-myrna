2300a48e2e50936f0642ded200b353bbb1c2d8ac
Naomi Gillis <naomigillis@Naomis-MacBook-Pro.local>
2024-01-22 17:53:46 -0500

    finished calendar

--- app/calendar/[[...calendar]]/page.tsx
+++ app/calendar/[[...calendar]]/page.tsx
@@ -2,14 +2,12 @@
 
 import { ThemeProvider } from '@mui/material/styles';
 import React from "react";
-// import CalendarModalButton from "../../components/CalendarModalButton";
 import Cal from "../../components/Calendar";
 import Sidebar from "../../components/Sidebar";
 import "../../globals.css";
 import theme from '../../theme';
 
 const Calendar: React.FC = () => {
-  // return (<ThemeProvider theme={theme}><Sidebar currentPageComponent={CalendarModalButton} /></ThemeProvider>)
   return (<ThemeProvider theme={theme}><Sidebar currentPageComponent={Cal} /></ThemeProvider>)
 };
 
--- app/components/Calendar.tsx
+++ app/components/Calendar.tsx
@@ -1,50 +1,85 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Unstable_Grid2';
-import {Typography} from '@mui/material';
+import { Typography } from '@mui/material';
 import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select';
+import moment from 'moment';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+const localizer = momentLocalizer(moment);
+
+const MyCalendar = (props) => {
+  const { events } = props; // Extract events from props
+
+  return (
+    <div style={{ height: '500px' }}>
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ margin: '50px' }}
+      />
+    </div>
+  );
+};
 
 const calendar = () => {
+  const events = [
+    {
+      start: new Date(2024, 0, 22, 10, 0),
+      end: new Date(2024, 0, 22, 12, 0),
+      title: 'Meeting with Client',
+    },
+    {
+      start: new Date(2024, 0, 23, 14, 0),
+      end: new Date(2024, 0, 23, 16, 0),
+      title: 'Team Workshop',
+    },
+  ];
+
   return (
     <Box
-    sx={{
-      display: 'flex',
-      minHeight: '90vh',
-      flexDirection: 'column', 
-      justifyContent: 'flex-start',
-      // alignItems: 'center', 
-      borderRadius: '5%',
-      backgroundColor: '#f6f6f6',
-      fontFamily: 'default',
-      margin: '2%',
-      marginLeft: '5%',
-      marginRight: '5%'
-    }}
-  >
-
-    <Grid container spacing={2} columns={20} paddingTop='2%'>
-      <Grid item xs={15} paddingBottom='5%'>
-        <Typography variant="h1" sx={{fontWeight: 'bold', paddingLeft:'8%', paddingTop:'5%'}}>
+      sx={{
+        display: 'flex',
+        minHeight: '90vh',
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+        borderRadius: '5%',
+        backgroundColor: '#f6f6f6',
+        fontFamily: 'default',
+        margin: '2%',
+        marginLeft: '5%',
+        marginRight: '5%',
+      }}
+    >
+      <Grid container spacing={2} columns={20} paddingTop="2%">
+        <Grid xs={15} paddingBottom="5%">
+          <Typography variant="h1" sx={{ fontWeight: 'bold', paddingLeft: '8%', paddingTop: '5%' }}>
             Calendar
-        </Typography>
+          </Typography>
+        </Grid>
+
+        <Grid xs={5} paddingTop="8%">
+          <Select
+            value=""
+            displayEmpty
+            sx={{ backgroundColor: '#FFFFFF', borderRadius: '10px', width: '200px', height: '38px' }}
+          >
+            <MenuItem value="" disabled>
+              Choose filters
+            </MenuItem>
+          </Select>
+        </Grid>
       </Grid>
 
-      <Grid item xs={5} paddingTop='8%'> 
-        <Select
-          value="" // Set the initial value to an empty string
-          displayEmpty // Display the selected value even when it's empty
-          sx={{ backgroundColor: '#FFFFFF', borderRadius: '10px', width: '200px', height: '38px'}}
-        >
-          <MenuItem value="" disabled>
-            Choose filters
-          </MenuItem>
-          {/* Add more MenuItem components with filter options here */}
-        </Select>
+      <Grid paddingBottom={'4%'}>
+        <MyCalendar events={events} />
       </Grid>
-    </Grid>
-    
-  </Box>)
-  }
+    </Box>
+  );
+};
 
 export default calendar;
