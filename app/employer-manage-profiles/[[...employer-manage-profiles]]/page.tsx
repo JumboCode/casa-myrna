dd8478a2a34b95a74e83c7db584a1dc4c0946b23
Carly Seigel <ceseigel@icloud.com>
2024-02-08 20:23:38 -0500

    finished admin view

--- app/employer-manage-profiles/[[...employer-manage-profiles]]/page.tsx
+++ app/employer-manage-profiles/[[...employer-manage-profiles]]/page.tsx
@@ -1,18 +1,56 @@
-'use client'
+"use client";
 
-import Sidebar from "../../components/Sidebar"; 
+import Sidebar from "../../components/Sidebar";
 import React, { useState, useEffect, ReactElement } from "react";
 import ManageProfiles from "../../components/ManageProfiles";
 import "../../globals.css";
-import { ThemeProvider } from '@mui/material/styles';
-import theme from '../../theme';
+import { ThemeProvider } from "@mui/material/styles";
+import theme from "../../theme";
 import { Protect } from "@clerk/nextjs";
+import { useUser } from "@clerk/nextjs";
 
-const employerManageProfiles: React.FC = ()  => {
-  return (<Protect> role="org:admin" fallback={<p>Only an admin can access this content.</p>}
-              <ThemeProvider theme={theme}><Sidebar currentPageComponent={ManageProfiles}/></ThemeProvider>
-          </Protect>)
+// const employerManageProfiles: React.FC = () => {
+//     const { isSignedIn, user, isLoaded } = useUser();
+//   if (2 + 2 == 4) {
+//     return (
+//       <ThemeProvider theme={theme}>
+//         <Sidebar currentPageComponent={ManageProfiles} />
+//       </ThemeProvider>
+//     );
+//   } else {
+//     return (
+//       <Protect
+//         role="org:admin"
+//         fallback={<p>Only an admin can access this content.</p>}
+//       >
+//         <ThemeProvider theme={theme}>
+//           <Sidebar currentPageComponent={ManageProfiles} />
+//         </ThemeProvider>
+//       </Protect>
+//     );
+//   }
+// };
+
+const employerManageProfiles: React.FC = () => {
+  const { isSignedIn, user, isLoaded } = useUser();
+  if (user?.publicMetadata.role == "Coordinator") {
+    return (
+      <ThemeProvider theme={theme}>
+        <Sidebar currentPageComponent={ManageProfiles} />
+      </ThemeProvider>
+    );
+  } else {
+    return (
+      <Protect
+        role="org:admin:employee"
+        fallback={<p>Only an admin or employee can access this content.</p>}
+      >
+        <ThemeProvider theme={theme}>
+          <Sidebar currentPageComponent={ManageProfiles} />
+        </ThemeProvider>
+      </Protect>
+    );
+  }
 };
 
 export default employerManageProfiles;
-
