31326be94cf65492c8c6919e72daa1d26e0a66ec
Nishika Pabba <nishika.pabba@gmail.com>
2024-05-02 13:51:17 -0400

    fixed main page user restrictions

--- app/page.tsx
+++ app/page.tsx
@@ -8,10 +8,34 @@ import "./globals.css";
 import theme from './theme';
 import { BrowserRouter as Router, useNavigate } from 'react-router-dom';
 import Cal from "./components/Calendar"
+import { Protect, useUser } from '@clerk/nextjs';
 
 
 const Main = () => {
-  return (<ThemeProvider theme={theme}><Router><Sidebar currentPageComponent={Cal}/></Router></ThemeProvider>)
+  const { isSignedIn, user, isLoaded } = useUser();
+  if (
+    user?.publicMetadata.role == "Coordinator" ||
+    user?.publicMetadata.role == "Full-time Staff" ||
+    user?.publicMetadata.role == "Part-time Staff" ||
+    user?.publicMetadata.role == "Relief Staff"
+  ) {
+    return (
+      <ThemeProvider theme={theme}>
+        <Sidebar currentPageComponent={Cal} />
+      </ThemeProvider>
+    );
+  } else {
+    return (
+      <Protect
+        role="org:admin:employee"
+        fallback={<p>Only an admin or employee can access this content.</p>}
+      >
+        <ThemeProvider theme={theme}>
+          <Sidebar currentPageComponent={Cal} />
+        </ThemeProvider>
+      </Protect>
+    );
+  }
 };
 
 export default Main;
